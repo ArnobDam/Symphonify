8be1b7f112f147e6078a08f2d7b0f1673891d285
Arnob <arnobdam@gmail.com>
2022-12-06 10:05:57 -0500

    forms' background color white, reorganized component structure

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -1,61 +1,45 @@
-import React, { useState } from 'react';
-import * as sessionActions from '../../store/session';
-import { useDispatch, useSelector } from 'react-redux';
-import { Redirect } from 'react-router-dom';
+import React from 'react';
+// import { useState } from 'react';
+// import * as sessionActions from '../../store/session';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { Redirect } from 'react-router-dom';
 import './LoginForm.css';
+import LoginForm from './LoginForm';
 
 function LoginFormPage() {
-  const dispatch = useDispatch();
-  const sessionUser = useSelector(state => state.session.user);
-  const [credential, setCredential] = useState('');
-  const [password, setPassword] = useState('');
-  const [errors, setErrors] = useState([]);
 
-  if (sessionUser) return <Redirect to="/" />;
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setErrors([]);
-    return dispatch(sessionActions.login({ credential, password }))
-      .catch(async (res) => {
-        let data;
-        try {
-          // .clone() essentially allows you to read the response body twice
-          data = await res.clone().json();
-        } catch {
-          data = await res.text(); // Will hit this case if the server is down
-        }
-        if (data?.errors) setErrors(data.errors);
-        else if (data) setErrors([data]);
-        else setErrors([res.statusText]);
-      });
-  }
+  // const LoginForm = () => (
+  //   <form onSubmit={handleSubmit}>
+  //     <ul>
+  //       {errors.map(error => <li key={error}>{error}</li>)}
+  //     </ul>
+  //     <label>
+  //       Username or Email
+  //       <input
+  //         type="text"
+  //         value={credential}
+  //         onChange={(e) => setCredential(e.target.value)}
+  //         required
+  //       />
+  //     </label>
+  //     <label>
+  //       Password
+  //       <input
+  //         type="password"
+  //         value={password}
+  //         onChange={(e) => setPassword(e.target.value)}
+  //         required
+  //       />
+  //     </label>
+  //     <button type="submit">Log In</button>
+  //   </form>
+  // )
 
   return (
-    <form onSubmit={handleSubmit}>
-      <ul>
-        {errors.map(error => <li key={error}>{error}</li>)}
-      </ul>
-      <label>
-        Username or Email
-        <input
-          type="text"
-          value={credential}
-          onChange={(e) => setCredential(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Password
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </label>
-      <button type="submit">Log In</button>
-    </form>
+    <div className="loginPage">
+      <LoginForm />
+    </div>
   );
 }
 
