3e3941f7f78d0efe5ab2e9668070fedab4e9d51d
Arnob <arnobdam@gmail.com>
2022-12-06 10:22:10 -0500

    added display flex to login / signup pages

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -8,34 +8,6 @@ import LoginForm from './LoginForm';
 
 function LoginFormPage() {
 
-
-  // const LoginForm = () => (
-  //   <form onSubmit={handleSubmit}>
-  //     <ul>
-  //       {errors.map(error => <li key={error}>{error}</li>)}
-  //     </ul>
-  //     <label>
-  //       Username or Email
-  //       <input
-  //         type="text"
-  //         value={credential}
-  //         onChange={(e) => setCredential(e.target.value)}
-  //         required
-  //       />
-  //     </label>
-  //     <label>
-  //       Password
-  //       <input
-  //         type="password"
-  //         value={password}
-  //         onChange={(e) => setPassword(e.target.value)}
-  //         required
-  //       />
-  //     </label>
-  //     <button type="submit">Log In</button>
-  //   </form>
-  // )
-
   return (
     <div className="loginPage">
       <LoginForm />
