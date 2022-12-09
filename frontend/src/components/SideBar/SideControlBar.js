d27bb10e8acef39e78f016d73bad8df8b103a756
Arnob <arnobdam@gmail.com>
2022-12-09 11:15:52 -0500

    added sidebar component file structures

---
+++ frontend/src/components/SideBar/SideControlBar.js
@@ -0,0 +1,7 @@
+
+
+const SideControlBar = () => {
+
+}
+
+export default SideControlBar;
---
+++ frontend/src/components/SideBar/index.js
@@ -0,0 +1,12 @@
+import './SideBar.css'
+import SideControlBar from './SideControlBar';
+
+function SideBar() {
+    return (
+        <>
+            <SideControlBar/>
+        </>
+    )
+}
+
+export default SideBar;
