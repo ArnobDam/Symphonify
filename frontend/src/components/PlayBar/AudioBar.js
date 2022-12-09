4965c0487e2a7efbd77e37b9278f6155a3469f7e
Arnob <arnobdam@gmail.com>
2022-12-08 21:57:41 -0500

    next previous buttons added, next button working, playbar works with sample playlist

--- frontend/src/components/PlayBar/AudioBar.js
+++ frontend/src/components/PlayBar/AudioBar.js
@@ -1,17 +1,53 @@
+import { useState } from 'react';
 import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
 import './PlayBar.css';
 
 // const AudioPlayer = window.ReactH5AudioPlayer.default;
 
+const playlist = [
+  { src: 'https://hanzluo.s3-us-west-1.amazonaws.com/music/ziyounvshen.mp3' },
+  { src: 'https://hanzluo.s3-us-west-1.amazonaws.com/music/wuyuwuqing.mp3' },
+  { src: 'https://hanzluo.s3-us-west-1.amazonaws.com/music/suipian.mp3' },
+]
+
 const AudioBar = () => {
+
+    const [currentTrack, setTrackIndex] = useState(0);
+    const handleClickNext = () => {
+        console.log('click next')
+        setTrackIndex((currentTrack) =>
+            currentTrack < playlist.length - 1 ? currentTrack + 1 : 0
+        );
+    };
+  
+    const handleEnd = () => {
+        console.log('end')
+        setTrackIndex((currentTrack) =>
+            currentTrack < playlist.length - 1 ? currentTrack + 1 : 0
+        );
+    }
+
     return (
+    // <AudioPlayer
+    //     className='bar'
+    //     // autoPlay
+    //     showSkipControls
+    //     src="https://download.samplelib.com/mp3/sample-12s.mp3"
+    //     // src="https://www.learningcontainer.com/wp-content/uploads/2020/02/Kalimba.mp3"
+    //     onPlay={e => console.log("onPlay")}
+
+    //     // other props here
+    // />
     <AudioPlayer
         className='bar'
         // autoPlay
-        src="https://download.samplelib.com/mp3/sample-12s.mp3"
+        showSkipControls
+        src={playlist[currentTrack].src}
         // src="https://www.learningcontainer.com/wp-content/uploads/2020/02/Kalimba.mp3"
         onPlay={e => console.log("onPlay")}
+        onClickNext={handleClickNext}
+        onEnded={handleEnd}
 
         // other props here
     />
