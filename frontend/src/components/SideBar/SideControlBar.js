5b4dadac70dae91a3280674eceb316801af7c72e
Arnob <arnobdam@gmail.com>
2022-12-09 14:47:24 -0500

    logo with title added to sidebar

--- frontend/src/components/SideBar/SideControlBar.js
+++ frontend/src/components/SideBar/SideControlBar.js
@@ -1,9 +1,12 @@
-
-
+// import spotifyLogo from "../../../../app/assets/images/spotify_white.png"
+import spotifyLogo from "./spotify_white.png"
 const SideControlBar = () => {
     return (
         <nav className="sideBar">
-
+            <div className="logoDiv">
+                <img className="logoIcon" src={spotifyLogo} />
+                <p className="logoName">Symphonify</p>
+            </div>
         </nav>
     )
 }
