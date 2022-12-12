1709efb1e9189de744ec7ba5e311f0ffe5604a01
Arnob <arnobdam@gmail.com>
2022-12-12 17:09:41 -0500

    sidebar logo turned into homepage link

--- frontend/src/components/SideBar/SideControlBar.js
+++ frontend/src/components/SideBar/SideControlBar.js
@@ -1,9 +1,12 @@
 // import spotifyLogo from "../../../../app/assets/images/spotify_white.png"
+import { useHistory } from "react-router-dom";
 import spotifyLogo from "./spotify_white.png"
 const SideControlBar = () => {
+    const history = useHistory();
+
     return (
         <nav className="sideBar">
-            <div className="logoDiv">
+            <div className="logoDiv" onClick={(e) => history.push(`/`)}>
                 <img className="logoIcon" src={spotifyLogo} />
                 <p className="logoName">Symphonify</p>
             </div>
