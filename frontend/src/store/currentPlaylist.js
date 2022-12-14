abcc8ffe6bd8e9b4b14087c32f534b3d831e67a3
Arnob <arnobdam@gmail.com>
2022-12-14 01:13:17 -0500

    troubleshooting previous albums songs playing on new album problem

--- frontend/src/store/currentPlaylist.js
+++ frontend/src/store/currentPlaylist.js
@@ -1,15 +1,18 @@
-export const RECEIVE_CURRENT_PLAYLIST = 'currentPlaylist/RECEIVE_CURRENT_PLAYLIST';
+// export const RECEIVE_CURRENT_PLAYLIST = 'currentPlaylist/RECEIVE_CURRENT_PLAYLIST';
 export const CREATE_CURRENT_PLAYLIST = 'currentPlaylist/CREATE_CURRENT_PLAYLIST';
+// export const DESTROY_CURRENT_PLAYLIST = 'currentPlaylist/DESTROY_CURRENT_PLAYLIST';
 
-export const receiveCurrentPlaylist = (currentPlaylist) => ({
-    type: RECEIVE_CURRENT_PLAYLIST,
-    currentPlaylist
-})
+// export const receiveCurrentPlaylist = (currentPlaylist) => ({
+//     type: RECEIVE_CURRENT_PLAYLIST,
+//     currentPlaylist
+// })
 
 export const createCurrentPlaylist = (songsArr) => ({
     type: CREATE_CURRENT_PLAYLIST,
     songsArr
 })
+
+// export const destroyCurrentPlaylist = ()
 
 export const makeCurrentPlaylist = (albumId) => async dispatch => {
     let response = await fetch(`/api/albums/${albumId}`);
@@ -22,10 +25,11 @@ const currentPlaylistReducer = (state = {}, action) => {
     const nextState = { ...state }
 
     switch (action.type) {
-        case RECEIVE_CURRENT_PLAYLIST:
-            nextState[action.currentPlaylist.id] = action.currentPlaylist;
-            return nextState;
+        // case RECEIVE_CURRENT_PLAYLIST:
+        //     nextState[action.currentPlaylist.id] = action.currentPlaylist;
+        //     return nextState;
         case CREATE_CURRENT_PLAYLIST:
+            // delete nextState[action];
             nextState[action.songsArr.album.id] = action.songsArr;
             return nextState;
         default:
