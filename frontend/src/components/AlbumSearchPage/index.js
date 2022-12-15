d2c4246758dcc4b0dca4950fa82a6822da1e95d3
Arnob <arnobdam@gmail.com>
2022-12-15 14:47:05 -0500

    fixed search bug

--- frontend/src/components/AlbumSearchPage/index.js
+++ frontend/src/components/AlbumSearchPage/index.js
@@ -1,22 +1,25 @@
 import React, { useEffect, useState } from "react";
 import AlbumList from "./AlbumList";
 import { useDispatch, useSelector } from "react-redux";
-import { useHistory } from "react-router-dom";
+// import { useHistory } from "react-router-dom";
 import { fetchAlbums } from "../../store/albums";
 import './AlbumSearchPage.css'
 
 function AlbumSearchPage() {
     const dispatch = useDispatch();
-    const history = useHistory(); 
+    // const history = useHistory(); 
     const albums = useSelector(state => state.albums ? Object.values(state.albums) : []);
-    const searchedAlbums = useSelector(state => state.searchedAlbums)
+    const searchedAlbums = useSelector(state => state.searchedAlbums ? state.searchedAlbums : [])
 
     // console.log(searchedAlbums)
 
+    // const
+
     const renderedAlbums = () => {
-        if (searchedAlbums.length === 0) {
+        if (JSON.stringify(searchedAlbums) === '{}' || searchedAlbums.length === 0) {
             return albums;
         } else {
+            // console.log(searchedAlbums)
             return searchedAlbums;
         }
     }
@@ -34,6 +37,8 @@ function AlbumSearchPage() {
             {/* <p>Albums</p> */}
             <AlbumList
                 albums={renderedAlbums()}
+                // albums={searchedAlbums}
+                // albums={albums}
                 highlightedAlbum={highlightedAlbum}
                 setHighlightedAlbum={setHighlightedAlbum}
             />
