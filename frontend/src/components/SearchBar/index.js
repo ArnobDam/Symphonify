65ae57ad0c84ef5d2531b4a58c5ff1dfa9174ccb
Arnob <arnobdam@gmail.com>
2022-12-15 11:02:26 -0500

    search bar functioning

--- frontend/src/components/SearchBar/index.js
+++ frontend/src/components/SearchBar/index.js
@@ -1,10 +1,54 @@
 import './SearchBar.css'
 import { FiSearch } from 'react-icons/fi';
+import { useSelector } from 'react-redux';
+import { useState } from 'react';
+
 
 
 function SearchBar() {
 
-    
+    const albums = useSelector(state => state.albums ? Object.values(state.albums) : []);
+
+    const [searchValue, setSearchValue] = useState("")
+
+    const [searchedAlbums, setSearchedAlbums] = useState([])
+    // console.log(albums)
+
+    const handleChange = (e) => {
+        setSearchValue(e.target.value)
+        
+        setSearchedAlbums([])
+        console.log(e.target.value)
+        if (e.target.value !== "") {
+            albums.forEach((album) => {
+                if (album.title.includes(e.target.value)) {
+                    console.log("hi")
+                    // if (!dupAlbums(searchedAlbums, album)) {
+                        setSearchedAlbums((searchedAlbums) => [...searchedAlbums, album])
+                    // }
+                    
+                }
+            })
+        }
+        
+    }
+
+    console.log(searchedAlbums)
+
+    const dupAlbums = (searchedAlbums, album) => {
+        searchedAlbums.forEach((searchedAlbum) => {
+                    
+            if (JSON.stringify(searchedAlbum) === JSON.stringify(album)) {
+                return true;
+            }
+        })
+
+        return false;
+    }
+
+    // console.log(searchedAlbums)
+
+    // console.log(searchValue)
 
     return (
         <>
@@ -13,8 +57,9 @@ function SearchBar() {
         <input type="text" 
         className='search'
         placeholder='What do you want to listen to?'
-        name="" 
-        // value=""
+        name=""
+        onChange={handleChange}
+        value={searchValue}
         />
         </>
     )
