c0f222d0302d54e57e33dee2c5b64807d4edc389
Arnob <arnobdam@gmail.com>
2023-01-11 20:52:20 -0500

    initialized playlist controller, index and show actions and vies

---
+++ app/controllers/api/playlists_controller.rb
@@ -0,0 +1,13 @@
+class Api::PlaylistsController < ApplicationController
+
+    def index
+        @playlists = Playlist.all
+        render :index
+    end
+
+    def show
+        @playlist = Playlist.find_by(id: params[:id])
+        render :show
+    end
+
+end
