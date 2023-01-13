eb608547810970a3f5ad33133781e1416107169d
Arnob <arnobdam@gmail.com>
2023-01-13 16:08:15 -0500

    initiliazed playlist index list component

---
+++ frontend/src/components/PlaylistIndexList/index.js
@@ -0,0 +1,17 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchPlaylists } from '../../store/playlists';
+import './PlaylistIndexList.css'
+
+function PlaylistIndexList() {
+    const dispatch = useDispatch();
+    const playlists = useSelector(state => state.playlists ? Object.values(state.playlists) : []);
+
+    useEffect(() => {
+        dispatch(fetchPlaylists())
+    }, [dispatch]);
+
+    
+};
+
+export default PlaylistIndexList;
