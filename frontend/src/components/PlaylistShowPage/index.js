7f8b835c79281a3c84cae28af4cc9eb2ca7bdd24
Arnob <arnobdam@gmail.com>
2023-01-12 22:51:49 -0500

    started playlist show page function

--- frontend/src/components/PlaylistShowPage/index.js
+++ frontend/src/components/PlaylistShowPage/index.js
@@ -1,6 +1,12 @@
-import './PlaylistShowPage.css' 
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import './PlaylistShowPage.css'
 
 function PlaylistShowPage() {
+    const dispatch = useDispatch();
+    const { playlistId } = useParams();
+
+    // const playlist = useSelector(state => state.playlists[playlistId] ? state.playlists[playlistId] : {})
 
 };
 
