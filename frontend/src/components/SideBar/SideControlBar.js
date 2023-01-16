6f0ec1321828169d760db3f8f37343577c31801e
Arnob <arnobdam@gmail.com>
2023-01-16 18:16:12 -0500

    dispatch new playlist

--- frontend/src/components/SideBar/SideControlBar.js
+++ frontend/src/components/SideBar/SideControlBar.js
@@ -6,10 +6,40 @@ import { BsPlusSquareFill } from 'react-icons/bs';
 import { useHistory } from "react-router-dom";
 import PlaylistIndexList from '../PlaylistIndexList';
 import spotifyLogo from "./spotify_white.png"
+import { useDispatch, useSelector } from 'react-redux';
+import { createPlaylist } from '../../store/playlists';
 
 
 const SideControlBar = () => {
+    const dispatch = useDispatch();
     const history = useHistory();
+    
+
+    const playlists = useSelector((state) => state.playlists ? Object.values(state.playlists) : []);
+    const session = useSelector(state => state.session ? state.session : {});
+
+    let numPlaylistsCurrentUser = 0;
+
+    playlists.forEach((playlist) => {
+        if (playlist.creatorId === session.user.id) {
+            numPlaylistsCurrentUser++;
+        }
+    })
+
+    // console.log(numPlaylistsCurrentUser);
+
+    const handleCreatePlaylist = () => {
+        debugger
+        dispatch(createPlaylist({
+            playlist: {
+                "title": `My Playlist #${numPlaylistsCurrentUser + 1}`,
+                "creatorId": session.user.id
+            }
+        }));
+        history.push(`/playlists/${playlists.length + 1}`);
+    };
+
+
 
     return (
         <nav className="sideBar">
@@ -26,7 +56,7 @@ const SideControlBar = () => {
                     <FiSearch className='search-icon'/>
                     <p className="search-text">Search</p>
                 </div>
-                <div className="create-playlist-div">
+                <div className="create-playlist-div" onClick={handleCreatePlaylist}>
                     <BsPlusSquareFill className='create-playlist-icon'/>
                     <p className="create-playlist-text">Create Playlist</p>
                 </div>
