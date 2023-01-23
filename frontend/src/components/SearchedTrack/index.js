18fa68163948e93630190d24378c797e0d1e75ec
Arnob <arnobdam@gmail.com>
2023-01-23 14:57:30 -0500

    cannot add duplicate track to playlist after refresh

--- frontend/src/components/SearchedTrack/index.js
+++ frontend/src/components/SearchedTrack/index.js
@@ -1,21 +1,39 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { createPlaylistSong } from '../../store/playlistSongs';
 import './SearchedTrack.css';
 
 function SearchedTrack({ id, songTitle, artistName, playlistId }) {
+    //playlistId is a string
     const dispatch = useDispatch();
     const history = useHistory();
+    
+
+    const playlistSongs = useSelector(state => state.playlistSongs ? state.playlistSongs : {});
+    const playlistSongsArr = Object.values(playlistSongs)
 
     const handleAddSongClick = () => {
-        dispatch(createPlaylistSong({
-            "playlist_id": playlistId,
-            "song_id": id
-        }));
+        let alreadyAdded = false;
+        
+        playlistSongsArr.forEach((playlistSong) => {
+            if (playlistSong.playlistId === parseInt(playlistId) && playlistSong.songId === id) {
+                alreadyAdded = true
+            }
+        })
+        if (!alreadyAdded) {
+            console.log("test")
+            debugger
+            dispatch(createPlaylistSong({
+                "playlist_id": playlistId,
+                "song_id": id
+            }));
+        }
+        
         // return (history.push(`/playlists/${playlistId}`));
     }
 
     return (
+        
         <div className='searched-track'>
                 <div className='searched-track-text'>
                     <p className='searched-track-song-title'>{songTitle}</p>
